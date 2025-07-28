e5e76a163631efd53b42778155ab7b92249bae63
WXH <841063834@qq.com>
2025-07-28 02:27:14 -0400

    module

---
+++ module/hello.c
@@ -0,0 +1,20 @@
+#include <linux/module.h> 
+#include <linux/init.h>   
+#include <linux/kernel.h>
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("wxh");
+MODULE_DESCRIPTION("hello world module");
+ 
+static int __init hello_init(void)
+{
+      printk(KERN_WARNING "yuan shen qi dong!\n");
+      return 0;
+}
+static void __exit hello_exit(void)
+{
+      printk(KERN_WARNING "hello exit!\n");
+}
+ 
+module_init(hello_init);
+module_exit(hello_exit);
